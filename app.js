8298dc5087a01612ff10960b92a8791633db2563
Nithish-123 <54845797+Nithish-123@users.noreply.github.com>
2020-07-29 11:37:00 +0530

    heroku setup

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const path = require("path");
 const express = require("express");
 app = express();
-
+const port = process.env.PORT;
 const pathfile = path.join(__dirname, "/src");
 console.log(pathfile);
 app.use(express.static(pathfile));
 app.get("", (req, res) => {
 	res.send("food recipe website");
 });
-app.listen(9000, () => {
+app.listen(port, () => {
 	console.log("server is up on port 4000");
 });
